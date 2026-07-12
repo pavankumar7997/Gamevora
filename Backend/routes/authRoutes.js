e1728bd54d624db563bf345d6f53099ad9f172e8
Pavan Kumar <narayanasettypavankumar@gmail.com>
2026-07-12 14:58:00 +0530

    Rename user.js to User.js

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const User = require("../models/User");
+const User = require("../models/user");
 
 const router = express.Router();
 router.get("/test", (req, res) => {
