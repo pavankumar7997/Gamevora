e6fa9dd1a12605243de9f827f822d93465f67606
Pavan Kumar <narayanasettypavankumar@gmail.com>
2026-07-15 21:49:34 +0530

    Added Firebase phone OTP signup

--- Frontend/js/firebase.js
+++ Frontend/js/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { getAUTH } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +18,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAnalytics(app);
+export const auth = getAuth(app);
